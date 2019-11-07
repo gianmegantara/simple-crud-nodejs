0891b191cddec76a39474c8061d17df56f974781
gianmegantara <gian48megantara@gmail.com>
2019-11-07 22:28:02 +0700

    add sequelize

--- util/database.js
+++ util/database.js
@@ -1,14 +1,3 @@
-//this connection is depend on your database it can be different
-// const mysql = require('mysql2')
-
-// const dbConnection = mysql.createPool({
-//     host: 'localhost', //hostname
-//     user: 'root', //username 
-//     password: 'tahugejrot', //password
-//     database: 'waw' //database name
-// })
-
-// module.exports = dbConnection.promise()
 const Sequelize = require('sequelize').Sequelize
 const sequelize = new Sequelize('waw', 'root', 'tahugejrot', {
     dialect: 'mysql',
